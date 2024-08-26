fa6e059f6e6abfd6af5e5fbb92764e5f2531c3b0
Jorge Developer <135453832+xxjoranxxl@users.noreply.github.com>
2024-08-26 17:35:07 -0500

    update

--- conexion.php
+++ conexion.php
@@ -1,11 +1,11 @@
 <?php
 	function conexion(){
 
-	$host = "dpg-cr6g015umphs73b4hn2g-a";
-	$port = "5432";
-	$dbname = "dbprueba_sffv";
-	$user = "dbprueba_sffv_user";
-	$password = "nMo5B20FWaBuaAvHEviYmwTFSEvFSgxU";
+	$host = "host=dpg-cr6g015umphs73b4hn2g-a";
+	$port = "port=5432";
+	$dbname = "dbname=dbprueba_sffv";
+	$user = "user=dbprueba_sffv_user";
+	$password = "password=nMo5B20FWaBuaAvHEviYmwTFSEvFSgxU";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
